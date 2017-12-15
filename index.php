c0f1a1387ca72311b2f7f00d8bdec2356ff7632a
btcraig <gnmiller@users.noreply.github.com>
2017-12-15 15:00:30 -0500

    fixing bug with ipv6 addrs

--- index.php
+++ index.php
@@ -8,7 +8,12 @@ URL to shorten: <input type="text" name="target" /><br>
 </html>
 <?php
     $out = shell_exec( "/usr/bin/python3.6 /var/www/html/backend/shorten.py ".$_POST["target"]." ".$_SERVER["REMOTE_ADDR"] ) ;
-    if(isset($_POST["send"])){
-        echo "Your shortened URL is -- ".$out;
+    if( isset( $_POST["send"] ) ) {
+        if( strpos($out,"ERR") != false ) {
+            echo "Failed to create short URL!\n";
+            echo $out;
+        } else {
+            echo "Your shortened URL is -- ".$out;
+        }
     }
 ?>
