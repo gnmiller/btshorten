8e736c1a0acc4606cc9ad6f6cc53a24ac8acf431
btcraig <gnmiller@users.noreply.github.com>
2017-12-15 13:45:18 -0500

    simple frontend working

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<html>
+<body>
+<form action="shorten.php" method="post">
+URL to shorten: <input type="text" name="target" /><br>
+<input type="submit" name="send" />
+</form>
+</body>
+</html>
 <?php
-echo "welcome!";
-$out = shell_exec( "/usr/bin/python3.6 /root/workspace/btshorten/shorten.py gtb.com ".$_SERVER['REMOTE_ADDR'] );
-echo $out;
+    $out = shell_exec( "/usr/bin/python3.6 /var/www/html/backend/shorten.py ".$_POST["target"]." ".$_SERVER["REMOTE_ADDR"] ) ;
+    if(isset($_POST["send"])){
+        echo "Your shortened URL is -- ".$out;
+    }
 ?>
