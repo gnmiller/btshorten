813be054f92d8ebaa5ae82a3bfc2d8e0562bbf6a
btcraig <gnmiller@users.noreply.github.com>
2017-12-18 10:51:39 -0500

    adding abs ref to "db.py"

--- shorten.py
+++ shorten.py
@@ -1,28 +1,23 @@
 #!/bin/python3
-import argparse, urllib3, hashlib, pymysql, sys, db, funcs
-# "macro" for file.seek()
-BEG = 0
-CUR = 1
-END = 2
+import argparse, urllib3, hashlib, pymysql, sys, funcs, logging, importlib.util
+spec = importlib.util.spec_from_file( "db.py", "/root/workspace/btshorten/db.py" )
+db = importlib.util.module_from_spec( spec )
+spec.loader.exec_module( db )
 
-# who needs safety
+BEG = 0; CUR = 1; END = 2
 urllib3.disable_warnings()
-
-# logging
-import logging
+base_url = "https://btcraig.in/"
+webroot = "/var/www/html/"
 log_level = logging.INFO
-logging.basicConfig( filename="/var/www/html/backend/shorten.log", level=log_level )
 
-# arg parsing...
 parser = argparse.ArgumentParser( prog="btshorten.py", description="Driver for btcraig.in link shortener." )
 parser.add_argument( "uri", metavar="link", help="The link to shorten. If http/https is not included http:// will be automatically added." )
 parser.add_argument( "ip", metavar="address", help="The IP address of the person attempting to create a shortened URI." )
 args = parser.parse_args()
-base_url = "https://btcraig.in/"
-webroot = "/var/www/html/"
+
+logging.basicConfig( filename="/var/www/html/backend/shorten.log", level=log_level )
 logging.debug( "URI :: {}\nIP :: {}\n\n".format( args.uri, args.ip ) )
 
-# add http if http/s not included and trim trailing '/'
 if not ( "http://" in args.uri or "https://" in args.uri ):
     uri = "http://{}".format( args.uri )
 else:
@@ -30,7 +25,6 @@ else:
 while args.uri[len(args.uri)-1] is '/':
     args.uri = args.uri[:len(args.uri)-1]
 
-# attempt to fetch the uri to check for 200
 try:
     http = urllib3.PoolManager()
     res = http.request( "HEAD", uri, timeout=.75 )
@@ -46,16 +40,12 @@ except urllib3.exceptions.MaxRetryError:
     logging.warning( "Failed to contact: {}".format( uri ) )
     print( "[ERR] Could not contact {}.".format( uri ) )
     sys.exit( -1 )
-
-# initial hash
 m = hashlib.md5()
 m.update( uri.encode() )
 hash = m.hexdigest()
 
-# check for collisions and insert
+conn = pymysql.connect( host=db.host, user=db.user, password=db.password, db=db.name, charset=db.cset, cursorclass=pymysql.cursors.DictCursor )
 try:
-    # connect to sql
-    conn = pymysql.connect( host=db.host, user=db.user, password=db.password, db=db.name, charset=db.cset, cursorclass=pymysql.cursors.DictCursor )
     with conn.cursor() as cursor:
         query = "SELECT * FROM uri WHERE hash_long RLIKE '{}'".format( hash )
         cursor.execute( query )
@@ -71,7 +61,6 @@ try:
         ins_query = "INSERT INTO uri (creator_ip, target, hash_short, hash_long) VALUES('{}', '{}', '{}', '{}')".format( args.ip, uri, hash[:6], hash )
         cursor.execute( ins_query )
         conn.commit()
-        # insert into .htaccess -- since we got a uniq hash we can safely insert
         with open( "{}.htaccess".format( webroot ), "a" ) as f:
             f.seek( 0, END )
             f.write( "Redirect 301 /{} {}\n".format( hash[:6], uri ) )
