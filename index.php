61852843b6d3654dc22abc2538f002402a2d56fb
btcraig <gnmiller@users.noreply.github.com>
2017-12-19 13:20:18 -0500

    fixing

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 <body>
 <form action="shorten.php" method="post">
 URL to shorten: <input type="text" name="target" /><br>
+Specify String: <input type="text" name="manual" /><br>
 <input type="submit" name="send" />
 </form>
 </body>
 </html>
 <?php
-    $out = shell_exec( "/usr/bin/python3.6 /var/www/html/backend/shorten.py '".$_POST["target"]."' '".$_SERVER["REMOTE_ADDR"]."'" ) ;
-    if( isset( $_POST["send"] ) ) {
-        if( strpos($out,"ERR") != false ) {
-            echo "Failed to create short URL!\n";
-            echo $out;
-        } else {
-            echo "Your shortened URL is -- ".$out;
-        }
+    $py = "/usr/bin/python3.6";
+    $shrt = "/var/www/html/backend/shorten.py";
+    $str = $py . " " . $shrt . " '" . $_POST["target"] . "' '" . $_SERVER["REMOTE_ADDR"] . "'";
+    $out = "";
+    if( isset( $_POST["manual"] ) && isset( $_POST["send"] ) ){
+        $out = shell_exec( $str . " -m " . $_POST["manual"] );
+    } elseif( isset( $_POST["send"] ) ){
+        $out = shell_exec( $py . " " . $shrt . " '" . $_POST["target"] . "' '" . $_SERVER["REMOTE_ADDR"] . "'" );
+    } else {
+        echo "Error!";
+    }
+    if( strpos($out,"ERR") != false ) {
+        echo "Failed to create short URL!\n";
+        echo $out;
+    } else {
+        echo "Your shortened URL is -- ".$out;
     }
 ?>
